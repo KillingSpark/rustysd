a7a69aa233f4af94ed6101b49a683876cd24db74
Moritz Borcherding <moritz.borcherding@web.de>
2019-12-11 20:24:24 +0100

    added todo

--- src/start_service.rs
+++ src/start_service.rs
@@ -272,6 +272,7 @@ fn after_fork_parent(
             ServiceType::Dbus => {
                 if let Some(dbus_name) = &conf.dbus_name {
                     trace!("[FORK_PARENT] Waiting for dbus name: {}", dbus_name);
+                    // TODO blocks forever right now for some reason
                     crate::dbus_wait::wait_for_name(&name).unwrap();
                     trace!("[FORK_PARENT] Found dbus name on bus: {}", dbus_name);
                 } else {
