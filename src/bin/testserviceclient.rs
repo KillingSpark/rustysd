c8225a6b0b8c89505deaf1e47e67db5453c0717d
Moritz Borcherding <moritz.borcherding@web.de>
2019-12-17 12:17:49 +0100

    connect to both sockets

--- src/bin/testserviceclient.rs
+++ src/bin/testserviceclient.rs
@@ -3,6 +3,7 @@ use std::os::unix::net::UnixDatagram;
 use std::os::unix::net::UnixStream;
 
 fn main() {
+    std::thread::spawn(move || {
     loop {
         let mut stream = UnixStream::connect("./sockets/servicelog_stream").unwrap();
         stream.write_all(b"AAAA\n").unwrap();
@@ -19,5 +20,23 @@ fn main() {
             .unwrap();
 
         std::thread::sleep(std::time::Duration::from_secs(2));
-    }
+    }   
+    });
+    loop {
+        let mut stream = UnixStream::connect("./sockets/servicelog_alt_stream").unwrap();
+        stream.write_all(b"DDDD\n").unwrap();
+        stream.write_all(b"EEEE\n").unwrap();
+        stream.write_all(b"FFFF\n").unwrap();
+        stream.write_all(b"This is a weird text protocol on the alternative socket").unwrap();
+        std::mem::drop(stream);
+
+        let socket_path = std::env::var("NOTIFY_SOCKET").unwrap();
+        let stream = UnixDatagram::unbound().unwrap();
+        stream.connect(socket_path).unwrap();
+        stream
+            .send(&b"STATUS=Sent a message to the server\n"[..])
+            .unwrap();
+
+        std::thread::sleep(std::time::Duration::from_secs(2));
+    }   
 }
