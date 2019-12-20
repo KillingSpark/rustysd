8599fbb8bcb6de5cc44ac15545e36ea1d6ae1bc0
Moritz Borcherding <moritz.borcherding@web.de>
2019-12-20 16:16:43 +0100

    line to compile for redox target

--- build_all.sh
+++ build_all.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 cargo build
 # cargo build --features linux_eventfd
+# cargo build --target x86_64-unknown-redox
+
 cd test_c_service
 clang -o test_service test_service.c $(pkg-config libsystemd --libs)
