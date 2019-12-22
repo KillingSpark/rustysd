f1b9987ae69c952d353dd7de341eb95e1599cc8e
Moritz Borcherding <moritz.borcherding@web.de>
2019-12-22 14:48:34 +0100

    use stripped release binaries for the docker image

--- dockerfiles/build_docker_image.sh
+++ dockerfiles/build_docker_image.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
-cargo build --target x86_64-unknown-linux-musl
+cargo build --target x86_64-unknown-linux-musl --release
+cd target/x86_64-unknown-linux-musl/release
+strip --strip-unneeded rustysd
+strip --strip-unneeded testservice
+strip --strip-unneeded testserviceclient
+cd ../../..
 docker build -t killingspark/rustysdtest -f dockerfiles/Dockerfile .
