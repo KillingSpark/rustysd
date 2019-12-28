bdec0456089d157919d74e7a99703349c507bb0d
Moritz Borcherding <moritz.borcherding@web.de>
2019-12-28 16:40:39 +0100

    move towards enabling timeouts for notificating services

--- src/services/fork_parent.rs
+++ src/services/fork_parent.rs
@@ -26,11 +26,34 @@ pub fn after_fork_parent(
                     &notify_socket_env_var
                 );
 
+                let start_time = std::time::Instant::now();
+                //let duration_timeout = Some(std::time::Duration::from_nanos(1_000_000_000_000));
+                let duration_timeout = None;
+                
                 let mut buf = [0u8; 512];
                 loop {
-                    let bytes = stream.recv(&mut buf[..]).unwrap();
+                    if let Some(duration_timeout) = duration_timeout {
+                        let duration_elapsed = start_time.elapsed();
+                        if duration_elapsed > duration_timeout {
+                            //TODO handle timeout correctly
+                            trace!("[FORK_PARENT] Service {} notification timed out", name);
+                            break;
+                        }else{
+                            let duration_till_timeout = duration_timeout - duration_elapsed;
+                            stream.set_read_timeout(Some(duration_till_timeout)).unwrap();
+                        }
+                    }
+                    let bytes = match stream.recv(&mut buf[..]) {
+                        Ok(bytes) => bytes,
+                        Err(e) => {
+                            match e.kind() {
+                                std::io::ErrorKind::WouldBlock => 0,
+                                _ => panic!("{}", e),
+                            }
+                        }
+                    };
                     srvc.notifications_buffer
-                        .push_str(&String::from_utf8(buf[..bytes].to_vec()).unwrap());
+                    .push_str(&String::from_utf8(buf[..bytes].to_vec()).unwrap());
                     crate::notification_handler::handle_notifications_from_buffer(srvc, &name);
                     if let ServiceStatus::Running = srvc.status {
                         trace!("[FORK_PARENT] Service {} sent READY=1 notification", name);
@@ -39,6 +62,7 @@ pub fn after_fork_parent(
                         trace!("[FORK_PARENT] Service {} still not ready", name);
                     }
                 }
+                stream.set_read_timeout(None).unwrap();
             }
             ServiceType::Simple => {
                 trace!("[FORK_PARENT] service {} doesnt notify", name);
