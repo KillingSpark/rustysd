8d448eff069ac9854ca543022e647957f4112692
Moritz Borcherding <moritz.borcherding@web.de>
2020-01-11 14:17:02 +0100

    consistent naming

--- src/platform/grnam.rs
+++ src/platform/grnam.rs
@@ -40,6 +40,6 @@ pub fn getgrnam(groupname: &str) -> Result<GroupEntry, String> {
 }
 
 #[cfg(not(target_os = "linux"))]
-pub fn getpwnam(username: &str) -> Result<PwEntry, String> {
+pub fn getgrnam(username: &str) -> Result<PwEntry, String> {
     Err("getpwnam is not yet implemented for this platform".into())
 }
