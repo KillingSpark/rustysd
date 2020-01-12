1592857215fad203fa250e35f1efc2b53cec661f
Moritz Borcherding <moritz.borcherding@web.de>
2020-01-12 20:02:46 +0100

    add _ for unused

--- src/platform/grnam.rs
+++ src/platform/grnam.rs
@@ -93,7 +93,7 @@ pub fn getgrnam_r(groupname: &str) -> Result<GroupEntry, String> {
 }
 
 #[cfg(not(any(target_os = "linux", target_os="freebsd")))]
-pub fn getgrnam_r(groupname: &str) -> Result<GroupEntry, String> {
+pub fn getgrnam_r(_groupname: &str) -> Result<GroupEntry, String> {
     Err("getpwnam_r is not yet implemented for this platform".into())
 }
 
