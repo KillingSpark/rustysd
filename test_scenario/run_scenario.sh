c4c0973af29ffd87b1e1534d1f163d9be89922b1
Moritz Borcherding <moritz.borcherding@web.de>
2020-02-11 09:35:55 +0100

    improve script that runs scenario

--- test_scenario/run_scenario.sh
+++ test_scenario/run_scenario.sh
@@ -1,2 +1,7 @@
+#! /bin/bash
 # This should print the output of the services ordered by the first number
-../target/debug/rustysd 2> /dev/null | grep ".service]"
+../target/debug/rustysd 2> /dev/null | grep ".service]" &
+
+sleep 5
+killall rustysd
+sleep 1
