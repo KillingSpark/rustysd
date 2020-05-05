98f86b792522575cb8ce9f0b42832ea9ca96de49
Moritz Borcherding <moritz.borcherding@web.de>
2020-05-05 20:37:55 +0200

    cargo fmt

--- src/units/unitset_manipulation/mod.rs
+++ src/units/unitset_manipulation/mod.rs
@@ -3,13 +3,13 @@
 mod activate;
 mod deactivate;
 mod insert_new;
+mod locking;
 mod remove;
 mod sanity_check;
-mod locking;
 
 pub use activate::*;
 pub use deactivate::*;
 pub use insert_new::*;
+pub use locking::*;
 pub use remove::*;
 pub use sanity_check::*;
-pub use locking::*;
