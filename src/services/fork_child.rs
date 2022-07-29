5202491e78c07477f58f964da4adf4d89678511b
Moritz Borcherding <moritz.borcherding@web.de>
2022-07-29 18:50:11 +0200

    document that we need to be careful about what we call in after_fork_child

--- src/services/fork_child.rs
+++ src/services/fork_child.rs
@@ -1,5 +1,56 @@
 use std::os::unix::io::RawFd;
 
+/// After forking we setup the all filedescriptors, move into a new process group and then exec the exec_helper
+/// 
+/// Note that this is called between fork and exec. This means this needs to be careful about what we call here!
+/// At least on linux this is a good reference: https://man7.org/linux/man-pages/man7/signal-safety.7.html
+pub fn after_fork_child(
+    selfpath: &std::ffi::CStr,
+    self_args: &[&std::ffi::CStr],
+    name: &str,
+    socket_fds: Vec<RawFd>,
+    new_stdout: RawFd,
+    new_stderr: RawFd,
+    exec_helper_config: RawFd,
+) {
+    // DO NOT USE THE LOGGER HERE. It aquires a global lock which might be held at the time of forking
+    // But since this is the only thread that is in the child process the lock will never be released!
+    //
+    // Also:
+    // The filedescriptor used by the logger might have been duped to another
+    // one and logging into that one would be.... bad
+    // Hopefully the close() means that no old logs will get written to that filedescriptor
+
+    // Setup the new stdio so println! and eprintln! go to the expected fds
+    dup_stdio(new_stdout, new_stderr, exec_helper_config);
+
+    // Lets move into a new process group before execing
+    move_into_new_process_group();
+
+    // Dup all the fds for the service here, because we use SO_CLOEXEC on all fds so doing it after exec isn't possible
+    if let Err(e) = dup_fds(name, socket_fds) {
+        eprintln!("[FORK_CHILD {}] error while duping fds: {}", name, e);
+        std::process::exit(1);
+    }
+
+    // Just so we have a clearer picture on what is happening while debugging
+    eprintln!("EXECV: {:?} {:?}", &selfpath, self_args);
+
+    // Finally exec the exec_helper
+    match nix::unistd::execv(selfpath, self_args) {
+        Ok(_) => {
+            eprintln!(
+                "[FORK_CHILD {}] execv returned Ok()... This should never happen",
+                name
+            );
+        }
+        Err(e) => {
+            eprintln!("[FORK_CHILD {}] execv errored: {:?}", name, e);
+            std::process::exit(1);
+        }
+    }
+}
+
 fn dup_stdio(new_stdout: RawFd, new_stderr: RawFd, exec_helper_config: RawFd) {
     // dup new stdout to fd 1. The other end of the pipe will be read from the service daemon
     let actual_new_fd = nix::unistd::dup2(new_stdout, libc::STDOUT_FILENO).unwrap();
@@ -20,6 +71,11 @@ fn dup_stdio(new_stdout: RawFd, new_stderr: RawFd, exec_helper_config: RawFd) {
 
     nix::unistd::dup2(exec_helper_config, libc::STDIN_FILENO).unwrap();
     nix::unistd::close(exec_helper_config).unwrap();
+}
+
+fn move_into_new_process_group() {
+    //make this process the process group leader
+    nix::unistd::setpgid(nix::unistd::getpid(), nix::unistd::Pid::from_raw(0)).unwrap();
 }
 
 fn dup_fds(name: &str, mut sockets: Vec<RawFd>) -> Result<(), String> {
@@ -66,56 +122,4 @@ fn dup_fds(name: &str, mut sockets: Vec<RawFd>) -> Result<(), String> {
         };
     }
     Ok(())
-}
-
-fn move_into_new_process_group() {
-    //make this process the process group leader
-    nix::unistd::setpgid(nix::unistd::getpid(), nix::unistd::Pid::from_raw(0)).unwrap();
-}
-
-pub fn after_fork_child(
-    selfpath: &std::ffi::CStr,
-    self_args: &[&std::ffi::CStr],
-    name: &str,
-    socket_fds: Vec<RawFd>,
-    new_stdout: RawFd,
-    new_stderr: RawFd,
-    exec_helper_config: RawFd,
-) {
-    // DO NOT USE THE LOGGER HERE. It aquires a global lock which might be held at the time of forking
-    // But since this is the only thread that is in the child process the lock will never be released!
-    //
-    // Also:
-    // The filedescriptor used by the logger might have been duped to another
-    // one and logging into that one would be.... bad
-    // Hopefully the close() means that no old logs will get written to that filedescriptor
-
-    // Setup the new stdio so println! and eprintln! go to the expected fds
-    dup_stdio(new_stdout, new_stderr, exec_helper_config);
-
-    // Lets move into a new process group before execing
-    move_into_new_process_group();
-
-    // Dup all the fds for the service here, because we use SO_CLOEXEC on all fds so doing it after exec isn't possible
-    if let Err(e) = dup_fds(name, socket_fds) {
-        eprintln!("[FORK_CHILD {}] error while duping fds: {}", name, e);
-        std::process::exit(1);
-    }
-
-    // Just so we have a clearer picture on what is happening while debugging
-    eprintln!("EXECV: {:?} {:?}", &selfpath, self_args);
-
-    // Finally exec the exec_helper
-    match nix::unistd::execv(selfpath, self_args) {
-        Ok(_) => {
-            eprintln!(
-                "[FORK_CHILD {}] execv returned Ok()... This should never happen",
-                name
-            );
-        }
-        Err(e) => {
-            eprintln!("[FORK_CHILD {}] execv errored: {:?}", name, e);
-            std::process::exit(1);
-        }
-    }
 }
